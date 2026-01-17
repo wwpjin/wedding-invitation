48403be9220794fc53634a9801b169c636491b26
jj <jj.noaled@gmail.com>
2026-01-17 13:54:39 +0900

    refactor: Update image assets by replacing PNGs with JPGs and adding new JPGs.

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,16 +1,28 @@
-import coverImage from "./cover.png"
-import image1 from "./image1.png"
-import image2 from "./image2.png"
-import image3 from "./image3.png"
-import image4 from "./image4.png"
-import image5 from "./image5.png"
-import image6 from "./image6.png"
-import image7 from "./image7.png"
-import image8 from "./image8.png"
-import image9 from "./image9.png"
-import image10 from "./image10.png"
-import image11 from "./image11.png"
-import image12 from "./image12.png"
+import coverImage from "./cover.JPG"
+import image1 from "./image1.JPG"
+import image2 from "./image2.JPG"
+import image3 from "./image3.JPG"
+import image4 from "./image4.JPG"
+import image5 from "./image5.JPG"
+import image6 from "./image6.JPG"
+import image7 from "./image7.JPG"
+import image8 from "./image8.JPG"
+import image9 from "./image9.JPG"
+import image10 from "./image10.JPG"
+import image11 from "./image11.JPG"
+import image12 from "./image12.JPG"
+import image13 from "./image13.JPG"
+import image14 from "./image14.JPG"
+import image15 from "./image15.JPG"
+import image16 from "./image16.JPG"
+import image17 from "./image17.JPG"
+import image18 from "./image18.JPG"
+import image19 from "./image19.JPG"
+import image20 from "./image20.JPG"
+import image21 from "./image21.JPG"
+import image22 from "./image22.JPG"
+import image23 from "./image23.JPG"
+
 
 export const COVER_IMAGE = coverImage
 
@@ -27,4 +39,15 @@ export const GALLERY_IMAGES = [
   image10,
   image11,
   image12,
+  image13,
+  image14,
+  image15,
+  image16,
+  image17,
+  image18,
+  image19,
+  image20,
+  image21,
+  image22,
+  image23,
 ]
