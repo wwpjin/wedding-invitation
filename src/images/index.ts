70cce57da7227b11d3ae6982230dfb9df0756620
jj <jj.noaled@gmail.com>
2026-01-19 11:04:14 +0900

    Swap image3 and image4 order

--- src/images/index.ts
+++ src/images/index.ts
@@ -1,8 +1,8 @@
 import coverImage from "./cover.webp"
 import image1 from "./image1.webp"
 import image2 from "./image2.webp"
-import image3 from "./image3.webp"
 import image4 from "./image4.webp"
+import image3 from "./image3.webp"
 import image5 from "./image5.webp"
 import image6 from "./image6.webp"
 import image8 from "./image8.webp"
@@ -28,8 +28,8 @@ export const COVER_IMAGE = coverImage
 export const GALLERY_IMAGES = [
   image1,
   image2,
-  image3,
   image4,
+  image3,
   image5,
   image6,
   image8,
