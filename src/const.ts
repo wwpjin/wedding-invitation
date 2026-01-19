089220ef1f43368496901e625f1425eec29080a2
jj <jj.noaled@gmail.com>
2026-01-19 17:16:24 +0900

    Update application constants.

--- src/const.ts
+++ src/const.ts
@@ -22,7 +22,7 @@ export const LOCATION_TEL = "010-9597-2215"
 
 // 카카오톡 공유 시 위치 정보로 사용할 주소.
 // LOCATION 과 동일하게 설정해도 무방하나, 필요에 따라 좀 더 상세히 작성 가능.
-export const SHARE_ADDRESS = LOCATION
+export const SHARE_ADDRESS = "모든날웨딩"
 export const SHARE_ADDRESS_TITLE = LOCATION
 
 // 네이버 지도 및 카카오 네비게이션에 사용할 좌표. [경도, 위도] 형식.
@@ -36,7 +36,7 @@ export const NMAP_PLACE_ID = 1616786350
 // 카카오 지도의 웨딩홀 장소 ID
 // 카카오 지도 웹페이지에서 웨딩홀 검색 후 해당 장소에서 상세보기 클릭 시 URL에서 확인 가능.
 // 예: https://place.map.kakao.com/8634826 -> 8634826
-export const KMAP_PLACE_ID = 8634826
+export const KMAP_PLACE_ID = 1150948700
 
 export const BRIDE_FULLNAME = "이혜숙"
 export const BRIDE_FIRSTNAME = "혜숙"
