a0bdfd293db81a45beac9f8d7e8ca56b5ea01cac
jj <jj.noaled@gmail.com>
2026-01-19 17:20:59 +0900

    chore: Update wedding date and add RSVP deadline constant.

--- src/const.ts
+++ src/const.ts
@@ -14,7 +14,7 @@ export const WEDDING_DATE_FORMAT = `YYYY년 MMMM D일 dddd A h시${WEDDING_DATE.
 
 // 예식 당월 휴무일. 켈린더에 표시하기 위함.
 // 예: 예식일 8월 -> 8월 15일 광복절
-export const HOLIDAYS = [15]
+export const HOLIDAYS = [16, 17, 18]
 
 export const LOCATION = "모든 날, Wedding"
 export const LOCATION_ADDRESS = "충청북도 청주시 남이면 석판리 379-4번지"
